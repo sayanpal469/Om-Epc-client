cd944a912fc47647994ea1810168005bda82eb77
sayanpal469 <sayanpal469@gmail.com>
2023-01-29 20:03:12 +0530

    react revel added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,110 +8,114 @@ import "swiper/css/navigation";
 import "./Banner.css";
 
 // import required modules
-import { Pagination, Autoplay} from "swiper";
+import { Pagination, Autoplay } from "swiper";
+
+import Flip from 'react-reveal/Flip';
 
 const Banner = () => {
-    return ( <div className="mt-0 banner">
-      <Swiper
-        direction={"vertical"}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        mousewheel={true}
-        modules={[Pagination, Autoplay]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className='slide-1 text-left bg-none'>
+  return (<div className="mt-0 banner">
+    <Swiper
+      direction={"vertical"}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      mousewheel={true}
+      modules={[Pagination, Autoplay]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <div className='slide-1 text-left bg-none'>
+          <div className="banner-content px-10">
+            <Flip right>
+              <h1 className='text-2xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, nulla. Voluptate voluptatibus eius qui maiores aperiam consequatur aliquid? Omnis, suscipit.</h1>
+            </Flip>
+            <div class="container mt-5">
+              <div class="button-container">
+                <span class="mask">Buy Now</span>
+                <button type="button" name="Hover">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='slide-2 text-left bg-none'>
           <div className="banner-content px-10">
             <h1 className='text-2xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, nulla. Voluptate voluptatibus eius qui maiores aperiam consequatur aliquid? Omnis, suscipit.</h1>
             <div class="container mt-5">
-      <div class="button-container">
-        <span class="mask">Buy Now</span>
-        <button type="button" name="Hover">Buy Now</button>
-      </div>
-    </div>
+              <div class="button-container">
+                <span class="mask">Buy Now</span>
+                <button type="button" name="Hover">Buy Now</button>
+              </div>
+            </div>
           </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-2 text-left bg-none'>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='slide-3 text-left bg-none'>
           <div className="banner-content px-10">
             <h1 className='text-2xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, nulla. Voluptate voluptatibus eius qui maiores aperiam consequatur aliquid? Omnis, suscipit.</h1>
             <div class="container mt-5">
-      <div class="button-container">
-        <span class="mask">Buy Now</span>
-        <button type="button" name="Hover">Buy Now</button>
-      </div>
-    </div>
+              <div class="button-container">
+                <span class="mask">Buy Now</span>
+                <button type="button" name="Hover">Buy Now</button>
+              </div>
+            </div>
           </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-3 text-left bg-none'>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='slide-4 text-left bg-none'>
           <div className="banner-content px-10">
             <h1 className='text-2xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, nulla. Voluptate voluptatibus eius qui maiores aperiam consequatur aliquid? Omnis, suscipit.</h1>
             <div class="container mt-5">
-      <div class="button-container">
-        <span class="mask">Buy Now</span>
-        <button type="button" name="Hover">Buy Now</button>
-      </div>
-    </div>
+              <div class="button-container">
+                <span class="mask">Buy Now</span>
+                <button type="button" name="Hover">Buy Now</button>
+              </div>
+            </div>
           </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-4 text-left bg-none'>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='slide-5 text-left bg-none'>
           <div className="banner-content px-10">
             <h1 className='text-2xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, nulla. Voluptate voluptatibus eius qui maiores aperiam consequatur aliquid? Omnis, suscipit.</h1>
             <div class="container mt-5">
-      <div class="button-container">
-        <span class="mask">Buy Now</span>
-        <button type="button" name="Hover">Buy Now</button>
-      </div>
-    </div>
+              <div class="button-container">
+                <span class="mask">Buy Now</span>
+                <button type="button" name="Hover">Buy Now</button>
+              </div>
+            </div>
           </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-5 text-left bg-none'>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='slide-6 text-left bg-none'>
           <div className="banner-content px-10">
             <h1 className='text-2xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, nulla. Voluptate voluptatibus eius qui maiores aperiam consequatur aliquid? Omnis, suscipit.</h1>
             <div class="container mt-5">
-      <div class="button-container">
-        <span class="mask">Buy Now</span>
-        <button type="button" name="Hover">Buy Now</button>
-      </div>
+              <div class="button-container">
+                <span class="mask">Buy Now</span>
+                <button type="button" name="Hover">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+    <div class="ocean">
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
     </div>
-          </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-6 text-left bg-none'>
-          <div className="banner-content px-10">
-            <h1 className='text-2xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, nulla. Voluptate voluptatibus eius qui maiores aperiam consequatur aliquid? Omnis, suscipit.</h1>
-            <div class="container mt-5">
-      <div class="button-container">
-        <span class="mask">Buy Now</span>
-        <button type="button" name="Hover">Buy Now</button>
-      </div>
-    </div>
-          </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <div class="ocean">
-  <div class="wave"></div>
-  <div class="wave"></div>
-  <div class="wave"></div>
-</div>
-      </div>
-    );
+  </div>
+  );
 };
 
 export default Banner;
--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Mission.css";
+import { AiFillCheckCircle } from 'react-icons/ai';
 
 const Mission = () => {
   return (
-    <div className="mission-container  grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 px-20">
+    <div className="mission-container grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 px-20">
       <div className="pb-20">
         <img
           className="max-w-full lg:h-[800px] h-auto"
@@ -12,13 +13,13 @@ const Mission = () => {
         />
       </div>
       <div>
-        <h1 className="text-4xl font-bold text-blue-400">
+        <h1 className="text-3xl font-semibold text-blue-600 text-left">
           Welcome To OM EPC Solution
         </h1>
-        <h2 className="text-6xl mt-5 text-black">
+        <h2 className="text-5xl font-bold mt-5 text-black">
           OM EPC - We are here to help you repair service
         </h2>
-        <p className="text-2xl text-black mt-10">
+        <p className="text-xl text-black mt-10">
           OM EPC Solution is dedicated to providing exceptional servicing and
           support for a wide range of computer, printer, CCTV camera, and UPS
           brands. Our mission is to be the trusted and reliable source for all
@@ -28,10 +29,38 @@ const Mission = () => {
           customer service and continuously improve our processes to meet the
           ever-evolving needs of the market.
         </p>
-        <div>
-            
+        <div className="mt-20 items-center justify-center grid lg:grid-cols-2 gap-5">
+          <div>
+            <div className="flex items-center gap-2 mt-5">
+              <AiFillCheckCircle className="text-blue-600  text-xl" />
+              <h1 className="text-lg">Quick Repair Process</h1>
+            </div>
+            <div className="flex items-center gap-2 mt-5">
+              <AiFillCheckCircle className="text-blue-600  text-xl" />
+              <h1 className="text-lg">Commitment to Customers</h1>
+            </div>
+            <div className="flex items-center gap-2 mt-5">
+              <AiFillCheckCircle className="text-blue-600  text-xl" />
+              <h1 className="text-lg">Low Price Guarantee</h1>
+            </div>
+            <div className="flex items-center gap-2 mt-5">
+              <AiFillCheckCircle className="text-blue-600  text-xl" />
+              <h1 className="text-lg">60 Days Warranty</h1>
+            </div>
+          </div>
+          <div>
+            <img className="max-w-full h-auto" src="https://i.ibb.co/NnBzPqv/pexels-elias-gamez-10558599.jpg" alt="" />
+          </div>
         </div>
-        <div></div>
+
+
+        <div>
+        {/* <div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  </div>
+</div> */}
+        </div>
       </div>
     </div>
   );
