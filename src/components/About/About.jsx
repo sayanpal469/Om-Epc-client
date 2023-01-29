9979dd2645202fca3a9ce7d25b05d16ee3db9012
unknown <dsubhojit063@gmail.com>
2023-01-29 10:05:19 +0530

    home banner

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
 
 const About = () => {
-    return (
-        <div>
-            About
-            About
-            About
-            About
-            About
+  return (
+    <>
+      <div class="landing-animation">
+        <div className="mx-10">
+        <h4 className="text-6xl text-center mb-20 text-white">OM EPC Solution</h4>
+        <p>OM EPC Solutions is a leading provider of comprehensive maintenance services for all types of computers, printers, cameras, batteries, and inverters. With its team of experienced technicians and their expertise in troubleshooting and fixing different types of electronic devices, the company is a trusted partner for individuals and businesses alike. The company offers an annual maintenance contract that covers regular check-ups and preventative maintenance, ensuring that all devices are functioning optimally at all times. In addition, the company provides 24/7 support for urgent repair needs, ensuring minimal downtime and interruption to the customer's operations.
+
+In today's digital age, electronic devices have become an integral part of our daily lives. Whether it's a home office setup or a large enterprise network, the functioning of these devices is critical to our personal and professional lives. However, even the most reliable devices can experience problems, and the downtime caused by these issues can be costly and disruptive. That's why it's important to have a reliable maintenance partner that can ensure the smooth functioning of these devices.
+
+OM EPC Solutions offers a wide range of services to its customers, including hardware repair, software upgrades, preventative maintenance, and 24/7 support. The company's technicians are trained in all the latest technology and are equipped with the latest tools and equipment to repair and maintain all types of devices. Whether it's a simple software upgrade or a complex hardware repair, the company's technicians are up to the task.
+
+In addition to its repair and maintenance services, OM EPC Solutions also offers training and support to its customers. The company's training programs are designed to help customers get the most out of their devices and to prevent problems from occurring in the first place. The company's support team is available 24/7 to answer any questions and to provide guidance and support whenever needed.</p>
         </div>
-    );
+        <div class="sine-wave">
+          <svg
+            class="svg-waves"
+            xmlns="http://www.w3.org/2000/svg"
+            xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28"
+            preserveAspectRatio="none"
+            shape-rendering="auto"
+          >
+            <defs>
+              <path
+                id="gentle-wave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+              ></path>
+            </defs>
+            <g class="svg-waves__parallax">
+              <use href="#gentle-wave" x="48" y="0"></use>
+              <use href="#gentle-wave" x="48" y="3"></use>
+              <use href="#gentle-wave" x="48" y="5"></use>
+              <use href="#gentle-wave" x="48" y="7"></use>
+            </g>
+          </svg>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -4,12 +4,12 @@ import './Nav.css'
 
 const Nav = ({children}) => {
     return (
-        <div className='mt-0'>
+        <div className='mt-0 nav-bar'>
             <div className="drawer">
                 <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content  flex flex-col">
                     {/* <!-- Navbar --> */}
-                    <div className="w-full navbar  bg-red-200">
+                    <div className="w-full navbar bg-black">
                         <div className="flex-none lg:hidden">
                             <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -21,9 +21,14 @@ const Nav = ({children}) => {
                         <div className="flex-none hidden lg:block">
                             <ul className="menu menu-horizontal">
                                 {/* <!-- Navbar menu content here --> */}
-                                <li><Link to='/'>Home</Link></li>
-                                <li><Link to='/Service'>Service</Link></li>
-                                <li><Link to='/about'>About</Link></li>
+                                <li className='font-medium'><Link to='/'>Home</Link></li>
+                                <li className='font-medium'><Link to='/about'>About</Link></li>
+                                <li className='font-medium'><Link to='/Service'>Service</Link></li>
+                                <li className='font-medium'><Link to='/Service'>UPS & Backup</Link></li>
+                                <li className='font-medium'><Link to='/Service'>Computer</Link></li>
+                                <li className='font-medium'><Link to='/Service'>Printer</Link></li>
+                                <li className='font-medium'><Link to='/Service'>Survillence</Link></li>
+                                <li className='font-medium'><Link to='/Service'>Career</Link></li>
                             </ul>
                         </div>
                     </div>
